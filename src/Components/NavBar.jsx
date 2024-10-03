29fb8d8e0bf6a924c8a7fb94534b44f5eb70dfa0
alexandrebiomed <65645417+alexandrebiomed@users.noreply.github.com>
2024-10-03 13:03:20 -0400

    Emphasize navbar header on current route

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/NavBar.css";
 
 function NavBar() {
     const navigate = useNavigate();
+    const location = useLocation(); // Get current location
 
     const loginRedirect = (event) => {
         event.preventDefault();
@@ -15,30 +16,24 @@ function NavBar() {
     }
 
     return (
-      <>
-        <div className="navbar container">
-            <div className="logo companyName">
-                <img src="images/logo2.png" alt="FamilyBlog logo" id="logo" />
-                <a href="/" id="familyblog">HappyFamily</a>
-            </div>
+      <div className="navbar container">
+          <div className="logo companyName">
+              <img src="images/logo2.png" alt="FamilyBlog logo" id="logo" />
+              <a href="/" id="familyblog">HappyFamily</a>
+          </div>
 
-            <div className="navigators">
-                <div id="Home"><a href="/">Home</a></div>
-                <div id="About"><a href="/about">About</a></div>
-                <div id="Contact Us"><a href="/contact">Contact Us</a></div>
-                <div id="Me"><a href="/team">The Team</a></div>
-            </div>
-                <div className="loggers">
-                    <button className="login" onClick={loginRedirect} type="submit">Log In</button>
-                    <button className="signup" onClick={signupRedirect} type="submit">Sign Up</button>
-                </div>
-        </div>
-      </>
-    )
-  }
-  
-  export default NavBar
-  
+          <div className="navigators">
+              <div className={location.pathname === '/' ? 'active' : ''} id="Home"><a href="/">Home</a></div>
+              <div className={location.pathname === '/about' ? 'active' : ''} id="About"><a href="/about">About</a></div>
+              <div className={location.pathname === '/contact' ? 'active' : ''} id="Contact Us"><a href="/contact">Contact Us</a></div>
+              <div className={location.pathname === '/team' ? 'active' : ''} id="Me"><a href="/team">The Team</a></div>
+          </div>
+          <div className="loggers">
+              <button className="login" onClick={loginRedirect} type="submit">Log In</button>
+              <button className="signup" onClick={signupRedirect} type="submit">Sign Up</button>
+          </div>
+      </div>
+    );
+}
 
-
-    
+export default NavBar;
